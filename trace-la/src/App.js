1fb1b860d4c14cbb748486e2c359240552144d3b
Ning Wang <ningwang1729@gmail.com>
2020-10-17 17:45:11 -0700

    set up a generic layout

---
+++ trace-la/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Trace LA Begins!
+        </p>
+        
+      </header>
+      <body>
+        <p>Display</p>
+      </body>
+      <footer>
+        <a
+          className="App-link"
+          href="https://www.creativelabsucla.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit CreativeLabs/TraceLA for more information
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
