7a72dbf9b6521e4235eef2b5d48c417651a7e38f
RogueArt <57082175+RogueArt@users.noreply.github.com>
2020-11-21 16:53:40 -0800

    Improved accessibility, fixed some UI glitches

---
+++ trace-la/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/header-footer.css'
+
+function footer() {
+  return (
+    <div>
+      <div className="footer" style={{width: '100vw'}}>
+        <div className="footer_body">
+          <a
+            className="link"
+            href="https://www.creativelabsucla.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CreativeLabs
+          </a>
+          <a className="link" href="#terms-of-service">Terms of Service</a>
+          <a className="link" href="#contact">Contact Information</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default footer;
